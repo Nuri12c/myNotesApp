f3d5cf1274a1810d47f71784bdcb2d62709a2b50
Nuri12c <nurislam.ganieiev@mail.ru>
2024-10-09 11:20:30 +0300

    editConfig

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-        ? '/notesApp/' // Замените `<REPO>` на имя вашего репозитория
+        ? '/myNotesApp/' // Замените `<REPO>` на имя вашего репозитория
         : '/'
 })
